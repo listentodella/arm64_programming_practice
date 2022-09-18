4bf0675baca790b12024aa1fe0c7efc4d4e7df52
listentodella <937138688@qq.com>
2022-09-18 18:05:19 +0800

    寻址方式: 基于基地址的寻址模式

--- mytest/chapter_03/lab03_1/src/kernel.c
+++ mytest/chapter_03/lab03_1/src/kernel.c
@@ -1,9 +1,20 @@
 #include "uart.h"
 
+extern void ldr_cmd_test();
+
+void my_ldr_str_cmd_test()
+{
+    // ldr cmd test
+    ldr_cmd_test();
+}
+
 void kernel_main(void)
 {
 	uart_init();
 	uart_send_string("Welcome BenOS!\r\n");
+
+    //my test
+    my_ldr_str_cmd_test();
 
 	while (1) {
 		uart_send(uart_recv());
