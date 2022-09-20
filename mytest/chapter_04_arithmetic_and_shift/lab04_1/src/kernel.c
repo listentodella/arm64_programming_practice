0dc7d4553f98cbddaa61eba60462b1fd29e23ed2
listentodella <937138688@qq.com>
2022-09-20 20:37:25 +0800

    sub 指令

--- mytest/chapter_04_arithmetic_and_shift/lab04_1/src/kernel.c
+++ mytest/chapter_04_arithmetic_and_shift/lab04_1/src/kernel.c
@@ -6,6 +6,9 @@ extern void add_shift_test();
 extern void adds_test();
 extern void adc_test();
 
+extern void sub_imm_test();
+extern void sub_reg_test();
+extern void sub_shift_test();
 
 void add_cmd_test()
 {
@@ -17,12 +20,22 @@ void add_cmd_test()
     adc_test();
 }
 
+void sub_cmd_test()
+{
+    sub_imm_test();
+    sub_reg_test();
+    sub_shift_test();
+}
+
+
 void kernel_main(void)
 {
 	uart_init();
 	uart_send_string("Welcome BenOS!\r\n");
 
     add_cmd_test();
+
+    sub_cmd_test();
 
 
 	while (1) {
