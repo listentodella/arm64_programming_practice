bd995eb0efd36a57e8a0e9d76d0a34831775c8ff
listentodella <937138688@qq.com>
2022-09-21 21:11:10 +0800

    cmn, csel指令

--- mytest/chapter_05_cmp_bl/src/kernel.c
+++ mytest/chapter_05_cmp_bl/src/kernel.c
@@ -1,9 +1,21 @@
 #include "uart.h"
 
+extern void cmn_cmd_test();
+extern void csel_cmd_test();
+
+void cmp_cmds_test()
+{
+    cmn_cmd_test();
+    csel_cmd_test(1, 3);
+}
+
 void kernel_main(void)
 {
 	uart_init();
 	uart_send_string("Welcome BenOS!\r\n");
+
+    cmp_cmds_test();
+
 
 	while (1) {
 		uart_send(uart_recv());
