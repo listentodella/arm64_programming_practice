57391a833a8493b82961898bffacd24f9800d980
listentodella <937138688@qq.com>
2022-09-21 21:16:11 +0800

    cset 指令

--- mytest/chapter_05_cmp_bl/src/kernel.c
+++ mytest/chapter_05_cmp_bl/src/kernel.c
@@ -2,11 +2,13 @@
 
 extern void cmn_cmd_test();
 extern void csel_cmd_test();
+extern void cset_cmd_test();
 
 void cmp_cmds_test()
 {
     cmn_cmd_test();
     csel_cmd_test(1, 3);
+    cset_cmd_test(2, 3);
 }
 
 void kernel_main(void)
