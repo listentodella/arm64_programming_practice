d2d65788cefed3248d050b32fe6b32aed84f1a5f
listentodella <937138688@qq.com>
2022-09-21 17:01:00 +0800

    C标志位的灵活运用

--- mytest/chapter_04_arithmetic_and_shift/lab04_2/src/kernel.c
+++ mytest/chapter_04_arithmetic_and_shift/lab04_2/src/kernel.c
@@ -1,9 +1,27 @@
 #include "uart.h"
 
+extern unsigned long compare_and_return_asm();
+
+unsigned long compare_and_return(unsigned long a, unsigned long b)
+{
+    if (a >= b) {
+        return 0;
+    } else {
+        return 0xFFFFFFFFFFFFFFFF;
+    }
+}
+
 void kernel_main(void)
 {
 	uart_init();
 	uart_send_string("Welcome BenOS!\r\n");
+
+    compare_and_return(6ull, 7ull);//return 0xFFF..FF
+    compare_and_return_asm(6ull, 7ull);//return 0
+
+    compare_and_return(7ull, 6ull);
+    compare_and_return_asm(7ull, 6ull);
+
 
 	while (1) {
 		uart_send(uart_recv());
