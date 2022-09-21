9ffbcd8eabc7743bba55d43fce89a88901034956
listentodella <937138688@qq.com>
2022-09-21 21:23:37 +0800

    csinc 指令

--- mytest/chapter_05_cmp_bl/src/kernel.c
+++ mytest/chapter_05_cmp_bl/src/kernel.c
@@ -3,12 +3,14 @@
 extern void cmn_cmd_test();
 extern void csel_cmd_test();
 extern void cset_cmd_test();
+extern void csinc_cmd_test();
 
 void cmp_cmds_test()
 {
     cmn_cmd_test();
     csel_cmd_test(1, 3);
     cset_cmd_test(2, 3);
+    csinc_cmd_test(4, 6);
 }
 
 void kernel_main(void)
