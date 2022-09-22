8ae186fb26e329e67e8b1229723a1b608508c2f8
listentodella <937138688@qq.com>
2022-09-22 16:53:35 +0800

    cmn lab 与 NZCV

--- mytest/chapter_05_cmp_bl/src/kernel.c
+++ mytest/chapter_05_cmp_bl/src/kernel.c
@@ -4,6 +4,10 @@ extern void cmn_cmd_test();
 extern void csel_cmd_test();
 extern void cset_cmd_test();
 extern void csinc_cmd_test();
+// 参数数据类型非常重要！如果填的不对或不合适、或者没填
+//那么最终传到寄存器里的值是会被裁剪的,位数不足,可能会进行无符号扩展
+//那么不仅会影响最终的计算结果，PSTATE受到的影响也不会按照预期进行！
+extern void cmn_lab_test(int long a, int long b);
 
 extern void b_ret_cmd_test();
 extern void b_ret_cmd_test2();
@@ -11,10 +15,13 @@ extern void b_ret_cmd_test3();
 
 void cmp_cmds_test()
 {
-    cmn_cmd_test();
-    csel_cmd_test(1, 3);
-    cset_cmd_test(2, 3);
-    csinc_cmd_test(4, 6);
+    // cmn_cmd_test();
+    // csel_cmd_test(1, 3);
+    // cset_cmd_test(2, 3);
+    // csinc_cmd_test(4, 6);
+
+    cmn_lab_test(1, -3);
+
 }
 
 void kernel_main(void)
