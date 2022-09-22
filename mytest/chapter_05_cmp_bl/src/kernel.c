488e57a596959368c22f7c93719d38533eaf65e6
listentodella <937138688@qq.com>
2022-09-22 15:31:33 +0800

    bl ret 的陷阱

--- mytest/chapter_05_cmp_bl/src/kernel.c
+++ mytest/chapter_05_cmp_bl/src/kernel.c
@@ -5,6 +5,10 @@ extern void csel_cmd_test();
 extern void cset_cmd_test();
 extern void csinc_cmd_test();
 
+extern void b_ret_cmd_test();
+extern void b_ret_cmd_test2();
+extern void b_ret_cmd_test3();
+
 void cmp_cmds_test()
 {
     cmn_cmd_test();
@@ -16,10 +20,14 @@ void cmp_cmds_test()
 void kernel_main(void)
 {
 	uart_init();
-	uart_send_string("Welcome BenOS!\r\n");
 
     cmp_cmds_test();
 
+    // b_ret_cmd_test();
+    b_ret_cmd_test2();
+    b_ret_cmd_test3();
+
+	uart_send_string("Welcome BenOS!\r\n");
 
 	while (1) {
 		uart_send(uart_recv());
