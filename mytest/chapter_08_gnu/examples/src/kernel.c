b35a6d50c8cfbec75e72e873cb5571c3c08130a5
listentodella <937138688@qq.com>
2022-10-01 19:50:06 +0800

    宏的伪指令:参数使用

--- mytest/chapter_08_gnu/examples/src/kernel.c
+++ mytest/chapter_08_gnu/examples/src/kernel.c
@@ -1,9 +1,13 @@
 #include "uart.h"
 
+extern void add_macro();
+
 void kernel_main(void)
 {
 	uart_init();
 	uart_send_string("Welcome BenOS!\r\n");
+
+    add_macro();
 
 	while (1) {
 		uart_send(uart_recv());
