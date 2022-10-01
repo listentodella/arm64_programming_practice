981bd33d41f077c96c45c2d5969ca1826bb8081f
listentodella <937138688@qq.com>
2022-10-01 20:59:59 +0800

    获取两个数中的较大数, C -> asm

--- mytest/chapter_08_gnu/labs/src/kernel.c
+++ mytest/chapter_08_gnu/labs/src/kernel.c
@@ -1,9 +1,15 @@
 #include "uart.h"
 
+extern void get_max_asm();
+extern void get_bigger_asm(unsigned long a, unsigned long b);
+
 void kernel_main(void)
 {
 	uart_init();
 	uart_send_string("Welcome BenOS!\r\n");
+
+    get_max_asm();
+    get_bigger_asm(2, 3);
 
 	while (1) {
 		uart_send(uart_recv());
