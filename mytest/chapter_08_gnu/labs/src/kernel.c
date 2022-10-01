8031af3d383101b94c5b92d290b181668f17de0c
listentodella <937138688@qq.com>
2022-10-01 21:14:36 +0800

    asm 调用 C

--- mytest/chapter_08_gnu/labs/src/kernel.c
+++ mytest/chapter_08_gnu/labs/src/kernel.c
@@ -1,16 +1,24 @@
 #include "uart.h"
 
 extern void get_max_asm();
-extern void get_bigger_asm(unsigned long a, unsigned long b);
+extern unsigned long get_bigger_asm(unsigned long a, unsigned long b);
+extern void c_call_me();
+
+unsigned long get_bigger(unsigned long a, unsigned long b)
+{
+    return a > b ? a:b;
+}
 
 void kernel_main(void)
 {
 	uart_init();
-	uart_send_string("Welcome BenOS!\r\n");
 
     get_max_asm();
     get_bigger_asm(2, 3);
 
+    c_call_me();
+
+	uart_send_string("Welcome BenOS!\r\n");
 	while (1) {
 		uart_send(uart_recv());
 	}
